1459bf34be0ac59313a87f2a5cf58ad32b14cc29
upendra <upendra1234@email.com>
2023-11-25 19:09:20 +0530

    -a

---
+++ dom.js
@@ -0,0 +1,12 @@
+//  dom mathod
+// 1-getinnertext
+ let a = document.getElementById("third").innerText;
+ console.log(a);
+//  2-getinnerhtml
+ let b = document.getElementById("third").innerHTML;
+ console.log(b);
+
+//  3-getattributes
+let c = document.getElementById("third").getAttributeNode("");
+console.log(c);
+
